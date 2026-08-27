698a72fdc8ddaeb636fd6afe4dd213f82af0e7f4
yboyg41 <yboyanimate@outlook.com>
2026-08-27 12:09:58 +0100

    modified:   assets/images/logo2.png modified:   assets/images/logo_moonie.png modified:   assets/images/logo_utroi.png modified:   assets/vectors/ybun_logo.svg modified:   index.html new file:   scripts/common/common_css.css new file:   scripts/common/common_el.html deleted:    scripts/common_css.css deleted:    scripts/common_el.html modified:   scripts/loading_commons.js modified:   style.css

--- scripts/loading_commons.js
+++ scripts/loading_commons.js
@@ -22,6 +22,6 @@ async function loadCommonCSS(url) {
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-    loadCommonEl("/scripts/common_el.html")
-    loadCommonCSS("/scripts/common_css.css")
+    loadCommonEl("/scripts/common/common_el.html")
+    loadCommonCSS("/scripts/common/common_css.css")
 })
