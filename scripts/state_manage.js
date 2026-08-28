844de1a2f11c5068a410562342e9a379ec8920f0
yboyg41 <yboyanimate@outlook.com>
2026-08-28 23:28:35 +0100

    modified:   index.html 	modified:   scripts/state_manage.js 	modified:   style/common_css.css 	modified:   style/landing_style.css 	new file:   style/portf_style.css

--- scripts/state_manage.js
+++ scripts/state_manage.js
@@ -1,11 +1,12 @@
+let state = "portf"
 
 document.addEventListener("DOMContentLoaded", () => {
-    let state = "landing"
+    
 
     let contentDiv = document.getElementById("content")
 
 
-    function loadPage(id) { 
+    function loadPage(id) {
         const elem = document.getElementById(id)
         const elemct = elem.content.cloneNode(true)
     
